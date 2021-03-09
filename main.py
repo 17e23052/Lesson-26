5f504e26e04fdfcb59681dcc8e549525c8def76c
Mathman <17e23052@marling.gloucs.sch.uk>
2021-03-09 12:03:52 +0000

    character checker

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-word = "sandwich"
-wordlength = len(word)
-guesses = 0
-print("Guess a word:")
-guess = input()
-while guesses < 5:
-  if guess == word:
-    print("Correct, you win!")
-    break
-  else:
-    print("Incorrect.")
-    guesses = guesses + 1
-    if guesses == 1:
-      print(f"The first letter of the word is {word[0]}")
-    elif guesses == 3:
-      print(f"The last letter of the word is {word[wordlength - 1]}")
-    elif guesses == 5:
-      print("You have had 5 incorrect guesses.")
-      print("You lose!")
-      break
-    print("Guess a word:")
-    guess = input()
+word = "sheep"
+count_e = 0
+print("What character do you want to check?")
+character = input()
+
+for letter in word:
+  if letter == character:
+    count_e = count_e + 1
+
+print(f"The letter {character} appears {count_e} time(s)")
